35a6890c2f6c507527a186b8e6a22f1c081f6609
Andriy Onufriyenko <7206130@gmail.com>
2021-05-04 20:41:10 +0300

    upd url in hw7

--- Homework_7/clean_folder/setup.py
+++ Homework_7/clean_folder/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(name='Cleaner',
       version='0.0.1',
       description='Clean up your computer',
-      url='http://github.com/dummy_user/useful',
+      url='https://github.com/uFitch/goit-python-homeworks/tree/main/Homework_7/clean_folder',
       author='Andriy Onufriyenko',
       author_email='7206130@gmail.com',
       license='MIT License',
