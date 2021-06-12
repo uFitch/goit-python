87074ef387c5b83690cd572fe749147555c072a2
Andriy Onufriyenko <7206130@gmail.com>
2021-06-12 22:35:48 +0300

    homework12

--- Homework_12/modul_12_Schoology.py
+++ Homework_12/modul_12_Schoology.py
@@ -1,36 +1,31 @@
-OPERATIONS = {
-    'hello': hello_func,
-    'add': hello_func,
-    'change': change_func,
-    'phone': phone_func,
-    'show all': show_all_func,
-    'good bye': exit_func,
-    'close': exit_func,
-    'exit': exit_func
-}
+import pickle
+import sys
+import os.path
+
+from classes import AddressBook, Record, Name, Phone
 
 
-phone_book = {}
+file = 'data.bin'
 
 
 def input_error(func):
-    def handler(data):
+    def hundler(data):
         try:
             result = func(data)
         except Exception as e:
             return e
         return result
-    return handler
+    return hundler
 
 
 @input_error
-def hello_func(data):
-    return 'How can I help you?'
+def hello(data):
+    print("How can I help you?")
 
 
 @input_error
 def add_func(data):
-    data = data.replace('add ', '')
+	data = data.replace('add ', '')
     if len(data.split()) == 2:
         name, phone = data.split()
         if name not in phone_book:
@@ -43,7 +38,7 @@ def add_func(data):
 
 @input_error
 def change_func(data):
-    data = data.replace('change ', '')
+	data = data.replace('change ', '')
     if len(data.split()) == 2:
         name, phone = data.split()
         if name in phone_book:
@@ -74,9 +69,30 @@ def show_all_func(data):
 
 
 @input_error
+def find(data):
+    data = data.replace('find ', '')
+    if len(data.split()) == 1:
+        result = phone_book.full_search(data)
+        return result
+
+
+@input_error
 def exit_func(data):
-    return'Good bye!'
-    break
+    with open(file, 'wb') as f:
+        pickle.dump(phone_book, f)
+    return "Good bye!"
+
+
+OPERATIONS = {
+    'hello': hello_func,
+    'add': hello_func,
+    'change': change_func,
+    'phone': phone_func,
+    'show all': show_all_func,
+    'good bye': exit_func,
+    'close': exit_func,
+    'exit': exit_func
+}
 
 
 @input_error
@@ -88,6 +104,10 @@ def choice_action(data):
 
 
 def main():
+    phone_book = AddressBook()
+    if os.path.isfile(file):
+        with open(file, 'rb') as f:
+            phone_book = pickle.load(f)
     while True:
         data = input()
 
@@ -96,13 +116,10 @@ def main():
             print(func)
             continue
         result = func(data)
-        if result == 'break':
-            break
-        elif result:
+        if result:
             print(result)
         if result == 'Good bye!':
             break
-
 
 if __name__ == '__main__':
     main()
