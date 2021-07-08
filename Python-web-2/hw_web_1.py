c3bd7ad098cd977f3488b53afe109f10ec85ddef
Andriy Onufriyenko <7206130@gmail.com>
2021-07-08 12:42:45 +0300

    add asset's in hw_web_1_1

--- Python-web-2/hw_web_1.py
+++ Python-web-2/hw_web_1.py
@@ -21,12 +21,12 @@ class SerializationJsonList(SerializationInterface):
         return json.loads(packed_data)
 
 
-class SerializationJsonDict(SerializationInterface):
+class SerializationJsonTuple(SerializationInterface):
     def serialize(self, data):
-        return json.dumps(data)
+        return json.dumps(list(data))
 
     def deserialize(self, packed_data):
-        return json.loads(packed_data)
+        return tuple(json.loads(packed_data))
 
 
 class SerializationJsonSet(SerializationInterface):
@@ -38,16 +38,15 @@ class SerializationJsonSet(SerializationInterface):
         for i, el in enumerate(x):
             if type(el) == list:
                 x[i] = tuple(el)
-        print(set(x), type(x))
         return set(x)
 
 
-class SerializationJsonTuple(SerializationInterface):
+class SerializationJsonDict(SerializationInterface):
     def serialize(self, data):
-        return json.dumps(list(data))
+        return json.dumps(data)
 
     def deserialize(self, packed_data):
-        return tuple(json.loads(packed_data))
+        return json.loads(packed_data)
 
 
 class SerializationBinList(SerializationInterface):
@@ -58,7 +57,15 @@ class SerializationBinList(SerializationInterface):
         return pickle.loads(packed_data)
 
 
-class SerializationBinList(SerializationInterface):
+class SerializationBinTuple(SerializationInterface):
+    def serialize(self, data):
+        return pickle.dumps(data)
+
+    def deserialize(self, packed_data):
+        return pickle.loads(packed_data)
+
+
+class SerializationBinSet(SerializationInterface):
     def serialize(self, data):
         return pickle.dumps(data)
 
@@ -74,17 +81,40 @@ class SerializationBinDict(SerializationInterface):
         return pickle.loads(packed_data)
 
 
-class SerializationBinSet(SerializationInterface):
-    def serialize(self, data):
-        return pickle.dumps(data)
+example_list = ["apple", "banana", "cherry"]
+example_tuple = ("apple", "banana", "cherry")
+example_set = {"apple", "banana", "cherry"}
+example_dict = {"brand": "Ford", "model": "Mustang", "year": 1964}
 
-    def deserialize(self, packed_data):
-        return pickle.loads(packed_data)
-        
 
-class SerializationBinTuple(SerializationInterface):
-    def serialize(self, data):
-        return pickle.dumps(data)
+packed_data = SerializationJsonList().serialize(example_list)
+des_data = SerializationJsonList().deserialize(packed_data)
+assert example_list == des_data
 
-    def deserialize(self, packed_data):
-        return pickle.loads(packed_data)
+packed_data = SerializationJsonTuple().serialize(example_tuple)
+des_data = SerializationJsonTuple().deserialize(packed_data)
+assert example_tuple == des_data
+
+packed_data = SerializationJsonSet().serialize(example_set)
+des_data = SerializationJsonSet().deserialize(packed_data)
+assert example_set == des_data
+
+packed_data = SerializationJsonDict().serialize(example_dict)
+des_data = SerializationJsonDict().deserialize(packed_data)
+assert example_dict == des_data
+
+packed_data = SerializationBinList().serialize(example_list)
+des_data = SerializationBinList().deserialize(packed_data)
+assert example_list == des_data
+
+packed_data = SerializationBinTuple().serialize(example_tuple)
+des_data = SerializationBinTuple().deserialize(packed_data)
+assert example_tuple == des_data
+
+packed_data = SerializationBinSet().serialize(example_set)
+des_data = SerializationBinSet().deserialize(packed_data)
+assert example_set == des_data
+
+packed_data = SerializationBinDict().serialize(example_dict)
+des_data = SerializationBinDict().deserialize(packed_data)
+assert example_dict == des_data
